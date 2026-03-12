a5a93fe3def9e3da979559225fa7adb79671c561
BH-Fang <d0972078975@gmail.com>
2026-03-12 14:25:43 +0800

    07 st

--- 114-2/07/07.c
+++ 114-2/07/07.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
-
+void p(int n, char c)
+{
+    for(int i = 0; i < n; i++)
+    {
+        printf("%c", c);
+    }
+}
 int main()
 {
-    
+    int N, M;
+    scanf("%d%d", &N, &M);
+    if(!(N >= 1 && N <= 2) || !(M >= 2 && M <= 9))
+    {
+        printf("error");
+        return 0;
+    }
+    switch(N)
+    {
+        case 1:
+            for(int i = 0; i < M; i++)
+            {
+                p(M - i - 1, '#');
+            }
+        case 2:
+    }
     return 0;
 }
