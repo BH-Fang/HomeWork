280d99a6fd44e3cc4cd3251908ba9a472eedf333
BH-Fang <d0972078975@gmail.com>
2026-03-19 23:19:40 +0800

    09 solved

--- 114-2/09/09.c
+++ 114-2/09/09.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
 #include <math.h>
 
-int G(int n, int k);
+void G(int n, int k, char result[], int pos)
+{
+    if(n == 1)
+    {
+        result[pos++] = k + '0';
+        result[pos] = '\0';
+    }
+    else if(k < 2 << (n - 2))
+    {
+        result[pos++] = '0';
+        G(n - 1, k, result, pos);
+    }
+    else
+    {
+        result[pos++] = '1';
+        G(n - 1, (2 << (n - 1)) - 1 - k, result, pos);
+    }
+}
 
 int main()
 {
-
+    int n, k;
+    scanf("%d", &n);
+    while (n != -1) 
+    {
+        scanf("%d", &k);
+        char result[10];
+        G(n, k, result, 0);
+        printf("%s\n", result);
+        scanf("%d", &n);
+    }
     return 0;
 }
 
-int G(int n, int k)
-{
-    if(n = 1)
-        return k;
-    if(k < pow(2, n - 1))
-        return 
-}
