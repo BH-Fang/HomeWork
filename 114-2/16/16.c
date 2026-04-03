0e1a6c9fdc894d85808e47adeab089f61afd859f
BH-Fang <d0972078975@gmail.com>
2026-04-03 17:55:35 +0800

    16 solved

--- 114-2/16/16.c
+++ 114-2/16/16.c
@@ -1,27 +1,103 @@
 #include <stdio.h>
 #include <string.h>
 
-void strToList(int *list, char *str, int *len)
+int getResult(int m, int len, int *list, int result[][10])
 {
-    for(int i = 0; str[i] != '\0'; i++)
+    int count = 0;
+    for(int i = 0; i < len - m + 1; i++)
     {
-        if(str[i] >= '0' && str[i] <= '9')
+        int S = 0, isValid = 1, t[10] = {0};
+        for(int j = 0; j < m; j++)
         {
-            list[i / 2] = str[i] -'0';
-            *len++;
+            int n = list[i + j];
+            if(S & (1 << n))
+            {
+                isValid = 0;
+                break;
+            }
+            else
+            {
+                t[j] = n;
+                S = S | (1 << n);
+            }
         }
+        if(isValid)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                result[count][j] = t[j];
+            }
+            count++;
+        }
+    }
+    return count;
+}
+
+void swapList(int m, int list2d[][10], int ind1, int ind2)
+{
+    int temp[10];
+    for(int i = 0; i < m; i++)
+    {
+        temp[i] = list2d[ind1][i];
+        list2d[ind1][i] = list2d[ind2][i];
+        list2d[ind2][i] = temp[i];        
+    }
+}
+
+int isList1BiggerThanList2(int m, int *list1, int *list2)
+{
+    for(int i = 0; i < m; i++)
+    {
+        if(list1[i] > list2[i]) 
+            return 1;
+        if(list1[i] < list2[i]) 
+            return 0;
+    }
+    return 2;
+}
+
+void sortResult(int m, int count, int result[][10])
+{   
+    for(int i = count - 1; i >= 0; i--)
+    {   
+        int has_sorted = 0;
+        for(int j = 0; j < i; j++)
+        if(isList1BiggerThanList2(m, result[j], result[j + 1]))
+        {
+            swapList(m, result, j, j+1);
+            has_sorted = 1;
+        }
+        if(!has_sorted)
+            break;   
     }
 }
 
 int main()
 {
-    int m, list[25], len = 0;
-    char getList[50];
+    int m, len = 0, list[30], result[100][10], count = 0;
 
-    scanf("%d", m);
-    fgets(getList, 50, stdin);
-    getList[strcspn(getList, "\n")] = '\0';
-    strToList(list, getList, &len);
+    scanf("%d", &m);
+    while(scanf("%d", &list[len]) == 1)
+    {
+        len++;
+        if(getchar() == '\n')
+            break;
+    }
+    
+    count = getResult(m, len, list, result);
+    sortResult(m, count, result);
+
+    printf("%d\n", count);
+    for(int i = 0; i < count; i++)
+    {
+        if(i != 0 && isList1BiggerThanList2(m, result[i], result[i - 1]) == 2)
+                continue; 
+        for(int j = 0; j < m; j++)
+        {    
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
